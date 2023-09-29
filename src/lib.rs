278ec4d22ddd611f6e2c9f7d15102b841d9a1622
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-09-29 12:30:44 +0100

    added event loop

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use polling::{Events, Poller, Event};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}, fs,
+};
+
+
+pub struct EventLoop;
+
+impl EventLoop {
+    pub fn eventLoop(socket: TcpListener) {
+        let poller = Poller::new().unwrap();
+        let key = 7;
+        unsafe {
+            let _add = poller.add(&socket, Event::readable(key)).unwrap();
+        }
+        let mut events = Events::new();
+        loop{
+            events.clear();
+            let _wait = poller.wait(&mut events, None).unwrap();
+            for ev in events.iter() {
+                if ev.key == key {
+                    // Perform a non-blocking accept operation.
+                    for stream in socket.incoming() { // the incoming method gives us sequence of streams
+                        let stream = stream.unwrap();
+                        Self::handle_connection(stream);
+                    }  
+                    // Set interest in the next readability event.
+                    let _modify = poller.modify(&socket, Event::readable(key)).unwrap();
+                }
+            }
+        }
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&mut stream); //adds buffering by managing calls to the std::io::Read trait methods
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
+        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+                                    
+        let contents = fs::read_to_string(filename).unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fs,
-};
+use std::net::TcpListener;
 
+use web_server::EventLoop;
 fn main() {
+   
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // start listening for tcp connection on port 7878
-    for stream in listener.incoming() { // the incoming method gives us sequence of streams
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }  
+    EventLoop::eventLoop(listener);
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream); //adds buffering by managing calls to the std::io::Read trait methods
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-                                
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-}
