18f7f5a9cf9152f09036e8f7f3d156c8f7bfc2b8
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-10-03 21:10:28 +0100

    thread pool and redirection

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,58 @@
-use polling::{Events, Poller, Event};
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}, fs,
-    thread
-};
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 
-pub struct EventLoop;
+pub struct ThreadPool{
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
-impl EventLoop {
-    pub fn eventLoop(socket: TcpListener) {
-        let poller = Poller::new().unwrap();
-        let key = 7;
-        unsafe {
-            let _add = poller.add(&socket, Event::readable(key)).unwrap();
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        let mut events = Events::new();
-        loop{
-            events.clear();
-            let _wait = poller.wait(&mut events, None).unwrap();
-            for ev in events.iter() {
-                if ev.key == key {
-                    // Perform a non-blocking accept operation.
-                    for stream in socket.incoming() { // the incoming method gives us sequence of streams
-                        let stream = stream.unwrap();
-                        thread::spawn(|| {
-                            Self::handle_connection(stream);
-                            println!("new thread initiated"); 
-                    });
-                    }  
-                    // Set interest in the next readability event.
-                    let _modify = poller.modify(&socket, Event::readable(key)).unwrap();
-                } else {
-                    println!("not valid key")
-                }
-            }
-        }
+
+        ThreadPool { workers, sender }
     }
 
-    fn handle_connection(mut stream: TcpStream) {
-        let buf_reader = BufReader::new(&mut stream); //adds buffering by managing calls to the std::io::Read trait methods
-        let request_line = buf_reader.lines().next().unwrap().unwrap();
-        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-            ("HTTP/1.1 200 OK", "index.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
-                                    
-        let contents = fs::read_to_string(filename).unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-        stream.write_all(response.as_bytes()).unwrap();
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
+
+        Worker { id, thread }
+    }
+
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use web_server::ThreadPool;
+//use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+//use std::thread;
+//use std::time::Duration;
 
-use web_server::EventLoop;
 fn main() {
-   std::env::set_var("RUST_BACKTRACE", "1");
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // start listening for tcp connection on port 7878
-    EventLoop::eventLoop(listener);
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    
 }
 
+fn handle_connection(mut stream: TcpStream) {
+    
+
+    let response = format!("HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:8080/\r\n\r\n");
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
