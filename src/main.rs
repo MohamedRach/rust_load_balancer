c7887997621bd5bab99c74b5fb8f8235ade38626
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-10-04 16:11:25 +0100

    sending get requests

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,26 @@ use std::net::ToSocketAddrs;
 use std::io::{prelude::*, BufReader};
 use std::net::TcpListener;
 use std::net::TcpStream;
-//use std::thread;
-//use std::time::Duration;
+use std::env;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
-
+    let args: Vec<String> = env::args().collect();
+    
+    thread::spawn(move || {
+        loop {
+            let query = &args[1];
+            let request_line = health_check("name");
+            println!("{}", request_line);
+            thread::sleep(Duration::from_secs(query.parse::<u64>().unwrap()));
+        }
+    });
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        health_check();
+        //health_check();
         pool.execute(|| {
             
             handle_connection(stream, String::from("http://localhost:8080"));
@@ -37,16 +47,17 @@ fn handle_connection(mut stream: TcpStream, server: String) {
 
 }
 
-fn health_check() {
+fn health_check(name: &str) -> String {
+    println!("{}: It's time!", name);
     let mut addrs_iter = "localhost:8080".to_socket_addrs().unwrap();
     let mut stream = TcpStream::connect(addrs_iter.next().unwrap()).unwrap();
             
     let header = format!("GET / HTTP/1.1\r\nHost: http://localhost:8080\r\nConnection: close\r\n\r\n");
-    let request = stream.write_all(header.as_bytes()).unwrap();         
+    let _request = stream.write_all(header.as_bytes()).unwrap();         
 
     // Make request and return response as string
     let buf = BufReader::new(&mut stream);
     let request_line = buf.lines().next().unwrap().unwrap();
             
-     println!("request line = {}", request_line);
+    request_line
 }
